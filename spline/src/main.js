544dfcdd9b963e2ca225fdb3611b7b8be55f1682
ArtoAvraham <143316684+ArtoAvraham@users.noreply.github.com>
2024-02-29 12:59:20 +0200

    Add files via upload

---
+++ spline/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/ysXhAOLKlAD61U9L/scene.splinecode');
